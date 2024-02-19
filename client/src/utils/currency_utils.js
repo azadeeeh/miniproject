f1f734a84380ba304953ac9be6177a04f156137f
azadeeeh <azadeeeh@yahoo.com>
2024-02-19 12:57:01 -0800

    part one added

---
+++ client/src/utils/currency_utils.js
@@ -0,0 +1,18 @@
+/**
+ * This file is meant to be where you will complete the utility function below, for performing a conversion of some amount
+ * to another currency.
+ */
+
+/**
+ * TODO:
+ * @receives two currency objects, currencyA and currencyB, as well as an integer-amount of cash in currencyA
+ * @performs a currency conversion between integer amount of currencyA to an integer amount of currencyB
+ * @returns an integer
+ */
+const convertCurrency = (currencyA, currencyB, amount) => {
+  // This needs to be implemented
+  const convertedAmount = amount * (currencyB.conversionRate / currencyA.conversionRate)
+  return convertedAmount
+}
+
+module.exports = convertCurrency
